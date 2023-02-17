c5bbb66d937a42c6a054fe1e768d44d000a7032f
jehoshua <jehoshuay@gmail.com>
2023-02-17 14:23:36 +0200

    project4

--- project.py
+++ project.py
@@ -1,30 +1,44 @@
 import os
+import re
 
-# Prompt the user for the Git URL
-git_url = input("Enter the Git URL: ")
+import git
 
-# Clone the repository
-os.system(f"git clone {git_url}")
+# Get git URL from environment variable
+url = os.environ.get('REPO_TO_CLONE')
 
-# Prompt the user for the branch to merge
-branch_name = input("Enter the name of the branch to merge: ")
-
-# Change to the repository directory
-repo_name = git_url.split("/")[-1].replace(".git", "")
-os.chdir(repo_name)
-
-# Checkout the specified branch
-os.system(f"git checkout {branch_name}")
-
-# Merge the specified branch into the current branch
-try:
-    os.system(f"git merge {branch_name}")
-except:
-    # If there are conflicts, run the diff tool
-    os.system("git mergetool")
-
-    # Complete the merge once conflicts are resolved
-    os.system("git merge --continue")
-
-# Delete the specified branch
-os.system(f"git branch -d {branch_name}")
+# Check if the URL is from GitHub
+if re.match(r"https://github.com/.*", url):
+    # Check if the repository exists locally
+    try:
+        repo = git.Repo("./NewRepo")
+    except git.exc.InvalidGitRepositoryError:
+        # Clone the repository
+        repo = git.Repo.clone_from(url, "./NewRepo")
+    except Exception as e:
+        print(e)
+        raise SystemExit
+      # Check if the branch exists, if not create it
+    try:
+        branch_name = os.environ.get('BRANCH_TO_MERGE')
+        branch = repo.create_head(branch_name)
+    except git.exc.GitCommandError:
+        branch = repo.heads[branch_name]
+    # Checkout the branch
+    branch.checkout()
+    # Attempt to merge the branch
+    try:
+        repo.git.merge("main")
+    except git.exc.GitCommandError as e:
+        if "CONFLICT" in e.stderr:
+            # Handle merge conflicts
+            print("Conflicts: ")
+            print(e.stderr)
+            resolve = input("Resolve conflicts and enter 'y' to continue: ")
+            if resolve == "y":
+                repo.git.add(A=True)
+                repo.index.commit("Resolved conflicts")
+    except Exception as e:
+        print(e)
+        raise SystemExit
+else:
+    print("you should use Github link only")
